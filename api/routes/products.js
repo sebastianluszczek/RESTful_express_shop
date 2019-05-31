998092aca150a08198721c506f3012b4d44379e6
Sebastian <luszczek.sebastian@awesome-ind.com>
2019-05-31 09:12:20 +0200

    env, models & mongoDB-Atlas connection

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/Product');
 
 router.get("/", (req, res, next) => {
   res.status(200).json({
@@ -8,10 +11,13 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-  const product = {
+  const product = new Product({
     name: req.body.name,
     price: req.body.price
-  }
+  });
+  product.save()
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
   res.status(201).json({
     message: "Handling POST request to /products",
     createdProduct: product
--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://utkow1991:${process.env.MONGO_ATLAS_PW}@cluster0-ympqd.mongodb.net/node_shop?retryWrites=true&w=majority`, {
+  useNewUrlParser: true
+});
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/order");
 
 app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {

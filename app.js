e0f6dd26e72f7b44588569fcec15850d735c3eef
Sebastian <luszczeksebastian@gmail.com>
2019-06-03 16:02:32 +0200

    '/app/...'

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/products", productRoutes);
-app.use("/orders", orderRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not found...");
